08aec5b0c0d5baf4c98191374312d4a089c22dec
Everest1508 <riteshmahale15@gmail.com>
2025-02-21 03:41:10 +0530

    first commit

--- waffel/db.php
+++ waffel/db.php
@@ -1,10 +1,10 @@
 <?php
-$servername = "mysql";
-$username = "user";
-$password = "password";
+$servername = "localhost";
+$username = "root";
+$password = "";
 $database = "waffle_shop";
 
-$conn = new mysqli($servername, $username, $password, $database, 3306);
+$conn = new mysqli($servername, $username, $password, $database);
 
 if ($conn->connect_error) {
     die("Database connection failed: " . $conn->connect_error);
